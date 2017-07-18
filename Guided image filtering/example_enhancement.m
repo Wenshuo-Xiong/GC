31ebf2fd865d07c8d72e2ae43c93d3afd29530dd
Wenshuo-Xiong <xiongwenshuo1@sina.com>
2017-07-18 16:50:49 +0800

    Create example_enhancement.m

---
+++ Guided image filtering/example_enhancement.m
@@ -0,0 +1,22 @@
+% example: detail enhancement
+% figure 6 in our paper
+
+close all;
+
+I = double(imread('.\img_enhancement\tulips.bmp')) / 255;
+p = I;
+
+r = 16;
+eps = 0.1^2;
+
+q = zeros(size(I));
+
+q(:, :, 1) = guidedfilter(I(:, :, 1), p(:, :, 1), r, eps);
+q(:, :, 2) = guidedfilter(I(:, :, 2), p(:, :, 2), r, eps);
+q(:, :, 3) = guidedfilter(I(:, :, 3), p(:, :, 3), r, eps);
+
+I_enhanced = (I - q) * 5 + q;		%边缘加强
+
+figure();
+imshow([I,   q   ,  I_enhanced], [0, 1]);
+	% 原图 滤波后   加强后
